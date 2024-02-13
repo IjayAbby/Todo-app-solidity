0a3555fa2fa0aef5fdff3b59a28801294f97f5ec
Ijay Abby <inyangasi4@gmail.com>
2024-02-13 15:39:22 +0100

    Add delete, updatestatus functions

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -19,4 +19,27 @@ contract TodoList {
         todo.description = _description;
         tasks.push(todo);
     }
+
+    function getTodo() external view returns (TodoItem[] memory) {
+        return tasks;
+    }
+
+    function updateTodo(
+        uint _index,
+        string memory _task,
+        string memory _description
+    ) public {
+        TodoItem storage todo = tasks[_index];
+        todo.task = _task;
+        todo.description = _description;
+    }
+
+    function updateStatus(uint _index) public {
+        TodoItem storage todo = tasks[_index];
+        todo.isDone = !todo.isDone;
+    }
+
+    function deleteTodo(uint _index) external {
+        delete tasks[_index];
+    }
 }
